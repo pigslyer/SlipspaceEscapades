3bc5cb924d26df435a9d3049793ef38de1dc23ac
pigslyer <63311292+pigslyer@users.noreply.github.com>
2022-12-11 11:52:30 +0100

    Added plasma balls and asteroid texture, fixed bullet speed

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,15 +1,15 @@
 class_name Bullet
 extends KinematicBody2D
 
-const SPEED := 50;
+const SPEED := 350;
 
 var velocity : Vector2;
 
 func _init(vel = Vector2.RIGHT):
 	velocity = vel;
 
-func _physics_process(_delta):
-	move_and_collide(velocity * SPEED);
+func _physics_process(delta):
+	move_and_collide(velocity * SPEED * delta);
 	
 	if(global_position.x > get_viewport_rect().size.x):
 		queue_free();
