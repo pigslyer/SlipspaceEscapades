35c559fc71fc446360865d3f1e3aeec6c47234e9
pigslyer <63311292+pigslyer@users.noreply.github.com>
2022-12-13 18:34:16 +0100

    added BFL dissipate, gun sound, fixed player rotation

--- Scripts/Powerups/BFL.gd
+++ Scripts/Powerups/BFL.gd
@@ -1,2 +1,12 @@
 class_name BFL
 extends Area2D
+
+const FADE_OUT_TIME = 2;
+
+func Disable():
+	set_deferred("collision_layer",0);
+	set_deferred("collision_mask",0);
+	
+	var tween := create_tween();
+	tween.tween_property(self, "scale:y", 0.0, FADE_OUT_TIME).set_trans(Tween.TRANS_CUBIC);
+	tween.tween_callback(self, "queue_free");
